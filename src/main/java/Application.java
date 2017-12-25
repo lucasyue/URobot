8de6cf72f8afd986d0747470d7d29bd4611c2421
lucasyue <fsh1281@163.com>
2017-12-25 18:07:21 +0800

    test

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -12,7 +12,7 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+    
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
